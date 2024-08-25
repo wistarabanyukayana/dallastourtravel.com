7745ce5c5c7533576f8233446aa02c2e5f09855b
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2024-08-26 06:52:18 +0700

    Update 260824

--- js/custom-flex.js
+++ js/custom-flex.js
@@ -1,23 +1,20 @@
-(function($) {
-	
-	"use strict";
-	
-	// Cache Selectors
-	var mainWindow		=$(window),
-		myBody			=$('body'),
-		mainSlider		=$('.flexslider');
-	
-	
-	// Flex Slider
-	mainWindow.on('load', function(){
-		mainSlider.flexslider({
-			animation: "slide",
-			start: function(slider){
-			  	myBody.removeClass('loading');
-			},
-			flexDirectionNav: false,
-			controlNav: false,
-		});
-	});
+(function ($) {
+  "use strict";
 
+  // Cache Selectors
+  var mainWindow = $(window),
+    myBody = $("body"),
+    mainSlider = $(".flexslider");
+
+  // Flex Slider
+  mainWindow.on("load", function () {
+    mainSlider.flexslider({
+      animation: "slide",
+      start: function (slider) {
+        myBody.removeClass("loading");
+      },
+      flexDirectionNav: false,
+      controlNav: false,
+    });
+  });
 })(jQuery);
